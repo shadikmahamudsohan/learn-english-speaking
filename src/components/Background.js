4c26792eda898544fbb3e72c7ce8a5a6e4485871
shadik <shadikmahamudsohan@gmail.com>
2022-09-28 01:12:47 +0600

    width fixed

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Background = ({ children }) => {
     return (
         <main className='min-h-screen bg-slate-900 flex justify-center items-center'>
-            <div class="min-w-max mx-auto rounded-md shadow-lg shadow-slate-200 px-20 py-10 bg-slate-50">
+            <div class="min-w-full md:min-w-max mx-auto rounded-md shadow-lg shadow-slate-200 px-20 py-10 bg-slate-50">
                 {children}
             </div>
         </main>
