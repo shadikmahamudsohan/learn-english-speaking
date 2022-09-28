eb45711f9dbe7f9493dd1de029a4c195f8211c70
shadik <shadikmahamudsohan@gmail.com>
2022-09-28 22:33:50 +0600

    listen to the text and listen to your voice added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Background from '../components/Background';
 import { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
+import { useAudioRecorder } from '@sarafhbk/react-audio-recorder';
 
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
@@ -15,18 +17,43 @@ const Home = () => {
             .then(data => setData(data));
     }, [start]);
 
+    const {
+        audioResult,
+        timer,
+        startRecording,
+        stopRecording,
+        pauseRecording,
+        resumeRecording,
+        status,
+        errorMessage
+    } = useAudioRecorder(); // voice record
+
 
     const {
         transcript,
         listening,
         browserSupportsSpeechRecognition
-    } = useSpeechRecognition();
+    } = useSpeechRecognition(); // turning speech into text
+
+    const { speak } = useSpeechSynthesis(); // listen text option
 
     useEffect(() => {
         if (listening === false) {
             setSpokenText(transcript);
         }
     }, [listening, transcript]);
+
+
+
+    useEffect(() => {
+        if (listening === false) {
+            console.log("stoped");
+            stopRecording();
+        }
+    }, [listening]);
+
+
+
 
     if (!browserSupportsSpeechRecognition) {
         return <span>Browser doesn't support speech recognition.</span>;
@@ -41,7 +68,7 @@ const Home = () => {
     const dataArray = [];
     if (data.quote && spokenText.length > 0) {
         for (let i = 0; i < getWords(data.quote.toLowerCase())?.length; i++) {
-            if (getWords(data.quote)[i].toLowerCase() !== getWords(spokenText)[i]) {
+            if (getWords(data.quote)[i].toLowerCase() !== getWords(spokenText)[i]?.toLowerCase()) {
                 dataArray.push(false);
             } else {
                 dataArray.push(true);
@@ -59,27 +86,47 @@ const Home = () => {
         return (parseInt(right) / parseInt(total)) * 100;
     }
     const correctPercentage = getPercentage(dataArray.length, allTrue.length);
-    console.log(correctPercentage);
+
+    console.log({ dataArray, text: getWords(data?.quote), myText: getWords(spokenText) });
     return (
         <Background>
+            {
+                data?.quote && <>
+                    <button
+                        className=' bg-blue-500 text-white mx-auto block mb-5'
+                        style={{ borderRadius: "50%", minWidth: "70px", minHeight: "70px" }}
+                        onClick={() => speak({ text: data.quote })}
+
+                    >Listen</button>
+                </>
+            }
+
+
             <h1 className='text-red-600 text-xl w-full md:w-96'>
                 {data.quote}
             </h1>
             <div className='mt-5'>
-                <p className='text-blue-600 text-xl w-full md:w-96'>{spokenText}</p>
+                <p className='mb-3 text-blue-600 text-xl w-full md:w-96'>{spokenText}</p>
                 <button className='py-5 md:py-2 w-full md:my-0 my-1 md:w-14 bg-blue-500 rounded mr-5 text-white'
                     onClick={() => {
                         SpeechRecognition.startListening();
                         setStart(start + 1);
+                        startRecording();
                     }
                     }>Start</button>
                 <button className='py-5 md:py-2 w-full md:my-0 my-1  md:w-14 bg-blue-500 rounded mr-5 text-white' onClick={SpeechRecognition.stopListening}>Stop</button>
-                <button className='py-5 md:py-2 w-full md:my-0 my-1  md:w-14 bg-blue-500 rounded mr-5 text-white' onClick={SpeechRecognition.startListening}>Retry</button>
+                <button className='py-5 md:py-2 w-full md:my-0 my-1  md:w-14 bg-blue-500 rounded mr-5 text-white'
+                    onClick={() => {
+                        SpeechRecognition.startListening();
+                        resumeRecording();
+                    }}
+                >Retry</button>
+                {(listening === false) && <audio className='w-full mt-5 border border-blue-500 rounded-3xl' controls src={audioResult} />}
 
                 <h1 className='text-5xl text-green-600 text-center my-5'>{(correctPercentage >= 0) && Math.floor(correctPercentage) + " %"}</h1>
 
+                {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
                 <p className='mt-3 font-xl'>{listening ? 'listening...' : ''}</p>
-
             </div>
         </Background>
     );
